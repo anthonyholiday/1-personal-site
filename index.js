ee4a51cece002ad786b70d7a9735e4c62ebd23be
Anthony Holiday <138096306+anthonyholiday@users.noreply.github.com>
2025-06-06 09:23:01 +0100

    Changed layout to be centered, background colours, styled the information as a card rather than just on the page.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// Challenge 2:
+
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("superhero")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("blueberry")
 
 // light, dark
-favouriteMode("dark")
+favouriteMode("light")
 
 // sharp, soft, round
 favouriteEdgeStyle("round")
